2f624b4a3bfe33f190414c9294459a10d953d902
20jun <ex_xe@naver.com>
2021-03-24 23:51:04 +0900

    #2.5 If, else, and, or

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
+const age = prompt("How old are you");
+
+if(age >= 18 && age <= 21) {
+    console.log('you can drink but you shiuld not');
+} else if(age>21){
+    console.log('you cant');
+} else {
+    console.log('lalal');
+}
+
+
+// && : 그리고
+// || : 또는
+
+
 // DOM : Document Object Model
 // DOM : Javascript가 html의 요소(태그)들을 Object로 가져온다.
 // Javascript에서 hmtl을 객체로 바꾸어서 변경할 수 있다.
 
 // querySelector는 노드의 첫 번째 자식을 반환한다.
-const title = document.querySelector("#title");
+// const title = document.querySelector("#title");
 
-function handleClick() {
-    title.style.color = "blue";
-}
-// handleResize() <<== 괄호 붙일 시 자동 호출
-// handleResize <<== 함수를 즉시 호출하지 않고 사이즈 변경시에만 호출
-title.addEventListener("click", handleClick);
+// function handleClick() {
+//     title.style.color = "blue";
+// }
+// // handleResize() <<== 괄호 붙일 시 자동 호출
+// // handleResize <<== 함수를 즉시 호출하지 않고 사이즈 변경시에만 호출
+// title.addEventListener("click", handleClick);
+
