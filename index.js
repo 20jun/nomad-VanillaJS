e2896d350b79d2b3c0518e42bd4a651dc98ce910
20jun <ex_xe@naver.com>
2021-03-31 00:41:56 +0900

    #2.6

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const age = prompt("How old are you");
+const title = document.querySelector("#title");
 
-if(age >= 18 && age <= 21) {
-    console.log('you can drink but you shiuld not');
-} else if(age>21){
-    console.log('you cant');
-} else {
-    console.log('lalal');
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if(currentColor === BASE_COLOR) {
+        title.style.color = OTHER_COLOR;
+    }
+    else {
+        title.style.color = BASE_COLOR;
+    }
 }
 
 
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+}
+init();
 // && : 그리고
 // || : 또는
-
 
 // DOM : Document Object Model
 // DOM : Javascript가 html의 요소(태그)들을 Object로 가져온다.
@@ -27,3 +37,4 @@ if(age >= 18 && age <= 21) {
 // // handleResize <<== 함수를 즉시 호출하지 않고 사이즈 변경시에만 호출
 // title.addEventListener("click", handleClick);
 
+// https://developer.mozilla.org/ko/docs/Web/Events 이벤트 참조
