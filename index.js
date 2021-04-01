3bbfde0021e4362fe58a46a13fb2baae71ddd4e4
20jun <ex_xe@naver.com>
2021-04-01 23:37:43 +0900

    #2.7

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)";
-const OTHER_COLOR = "#7f8c8d";
-
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-    const currentColor = title.style.color;
-    if(currentColor === BASE_COLOR) {
-        title.style.color = OTHER_COLOR;
-    }
-    else {
-        title.style.color = BASE_COLOR;
-    }
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+        title.classList.toggle(CLICKED_CLASS);
 }
 
-
 function init() {
-    title.style.color = BASE_COLOR;
     title.addEventListener("click", handleClick);
 }
 init();
+
+
 // && : 그리고
 // || : 또는
 
